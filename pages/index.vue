893e10c0e7cd3134b9de17bf19936316e8db4c27
Mr. Redpolarbear <mrredpolarbear@gmail.com>
2017-12-18 14:46:44 -0800

    layout

--- pages/index.vue
+++ pages/index.vue
@@ -16,24 +16,38 @@
         <b-button type="submit" variant="primary">Submit</b-button>
       </b-row>
     </b-form>
-    <pre>
-      {{ item }}
-    </pre>
     <b-card>
-      <b-row align-h="between">
-        <b-col>{{ item.name }}</b-col>
-      </b-row>
-      <b-row fluid class="text-center">
-        <b-col align-self="center" v-for="(img, index) in item.imgs" :key="index">
-          <b-img thumbnail fluid :src="img" style="max-width: 128px; max-height: 128px;"/>
+      <b-row>
+        <b-col>
+          <h5>{{ item.name }}</h5>
         </b-col>
       </b-row>
-      <b-card v-for="(sku, indx) in item.skus" :key="indx">
-        <b-media>
-          <b-img slot="aside" :src="sku.colorImg" width="64" alt="placeholder" />
-          <h5 class="mt-0">{{ sku.colorName }}</h5>
-        </b-media>
-      </b-card>
+      <b-row fluid no-gutters>
+        <b-col align-self="center" v-for="(img, index) in item.imgs" :key="index">
+          <b-img fluid :src="img" style="max-width: 128px; max-height: 128px;"/>
+        </b-col>
+      </b-row>
+      <b-row fluid>
+        <b-col cols="4" v-for="(sku, indx) in item.skus" :key="indx">
+          <b-card>
+            <b-media>
+              <b-img v-if="sku.colorImg" slot="aside" :src="sku.colorImg" width="64" height="64" />
+              <b-img v-else slot="aside" blank blank-color="#abc" width="64" height="64" />
+              <p class="mt-0">Color: {{ sku.colorName }}</p>
+              <p>Price: {{ sku.price }}</p>
+              <b-form-group id="sizeSelectGroup" label="Size:" label-for="sizeSelect">
+                <!-- <label class="mr-sm-2" for="sizeSelect">Size: </label> -->
+                <b-form-select id="sizeSelect" v-if="sku.sizes" :select-size="4">
+                  <option v-for="size in sku.sizes" :key="size" :value="size">{{ size }}</option>
+                </b-form-select>
+                <b-form-select id="sizeSelect" v-else>
+                  <option value="One Size">One Size</option>
+                </b-form-select>
+              </b-form-group>
+            </b-media>
+          </b-card>
+        </b-col>
+      </b-row>
       <b-card no-body>
         <b-tabs card>
           <b-tab title="DESCRIPTION" active>
@@ -42,7 +56,9 @@
         </b-tabs>
       </b-card>
     </b-card>
-
+    <!-- <pre>
+      {{ item }}
+    </pre> -->
   </b-container>
 </template>
 
